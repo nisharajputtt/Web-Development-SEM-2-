bdc06fa8512e314a863ca8943fdc9bea5b72fcb5
nisha rajput <nishur008@gmail.com>
2026-05-26 13:22:44 +0530

    done

---
+++ experiment 9/src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+function App(){
+  const [name,setName]= useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("";)
+}
